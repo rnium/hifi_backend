56288c9c15d6a6afde636d6362151005858408d1
HifiComputer <rniumo@gmail.com>
2024-07-16 16:55:12 +0600

    custom pagination

--- hificom/api/pagination.py
+++ hificom/api/pagination.py
@@ -1,5 +1,13 @@
 from rest_framework.pagination import PageNumberPagination
-
+from rest_framework.response import Response
 
 class ProductsPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 20
+
+    def get_paginated_response(self, data):
+        return Response({
+            'current_page': self.page.number,
+            'total_pages': self.page.paginator.num_pages,
+            'per_page': self.page.paginator.per_page,
+            'results': data,
+        })
