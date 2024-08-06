a76fec6d84ec7ae31dc6f615c05ed128bfcbe0e5
HifiComputer <rniumo@gmail.com>
2024-08-06 13:08:02 +0600

    power supply

---
+++ hificom/management/commands/categories/power_supply.py
@@ -0,0 +1,182 @@
+power_supply_brands = [
+    {
+        'title': 'Gigabyte',
+        'slug': 'gigabyte-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'Gigabyte Power Supplies at HiFi Computer',
+        'description': 'Explore Gigabyte power supplies at HiFi Computer, known for their reliability and performance. Ideal for gaming and professional setups, Gigabyte power supplies offer stable power delivery and high efficiency. Ensure your PC runs smoothly with a quality Gigabyte power supply. Shop now for competitive prices and enjoy doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'MSI',
+        'slug': 'msi-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'MSI Power Supplies at HiFi Computer',
+        'description': 'Discover MSI power supplies at HiFi Computer, perfect for high-performance PCs. MSI power supplies are designed to provide stable and efficient power delivery, ensuring your system runs smoothly under heavy loads. Upgrade your PC with a reliable MSI power supply. Shop now for competitive prices and enjoy doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'ValueTop',
+        'slug': 'value-top-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'ValueTop Power Supplies at HiFi Computer',
+        'description': 'Check out ValueTop power supplies at HiFi Computer, where affordability meets quality. These power supplies are designed to offer reliable performance without breaking the bank. Perfect for budget-conscious builders, ValueTop power supplies ensure stable power delivery for your PC. Enjoy competitive prices and convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'Gamdias',
+        'slug': 'gamdias-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'Gamdias Power Supplies at HiFi Computer',
+        'description': 'Shop Gamdias power supplies at HiFi Computer for reliable and efficient power solutions. Gamdias power supplies are known for their durability and stable power delivery, making them ideal for gaming and professional setups. Enhance your PC with a quality Gamdias power supply. Enjoy competitive prices and doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'FSP',
+        'slug': 'fsp-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'FSP Power Supplies at HiFi Computer',
+        'description': 'Discover FSP power supplies at HiFi Computer, offering high efficiency and reliable performance. FSP power supplies are designed to provide stable power delivery, ensuring your system runs smoothly and efficiently. Upgrade your PC with a trusted FSP power supply. Shop now for competitive prices and enjoy doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'Deepcool',
+        'slug': 'deepcool-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'Deepcool Power Supplies at HiFi Computer',
+        'description': 'Explore Deepcool power supplies at HiFi Computer, perfect for cooling and power efficiency. Deepcool power supplies are designed to provide stable power delivery and effective cooling solutions for your PC. Ensure your system runs smoothly with a quality Deepcool power supply. Enjoy competitive prices and doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'Corsair',
+        'slug': 'corsair-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'Corsair Power Supplies at HiFi Computer',
+        'description': 'Check out Corsair power supplies at HiFi Computer, known for their high performance and reliability. Corsair power supplies offer stable power delivery and high efficiency, making them ideal for gaming and professional setups. Enhance your PC with a trusted Corsair power supply. Shop now for competitive prices and enjoy doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'Antec',
+        'slug': 'antec-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'Antec Power Supplies at HiFi Computer',
+        'description': 'Shop Antec power supplies at HiFi Computer for reliable and efficient power solutions. Antec power supplies are designed to provide stable power delivery and high efficiency, ensuring your PC runs smoothly under heavy loads. Upgrade your system with a quality Antec power supply. Enjoy competitive prices and doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'Cooler Master',
+        'slug': 'cooler-master-powersupply',
+        'cat_type': 'brand',
+        'priority': 4,
+        'seo_title': 'Cooler Master Power Supplies at HiFi Computer',
+        'description': 'Discover Cooler Master power supplies at HiFi Computer, perfect for high-performance PCs. Cooler Master power supplies offer stable power delivery and effective cooling solutions, making them ideal for gaming and professional setups. Ensure your PC runs efficiently with a quality Cooler Master power supply. Shop now for competitive prices and enjoy doorstep delivery across Bangladesh.'
+    }
+]
+
+power_supply_wattage = [
+    {
+        'title': 'Up to 500W',
+        'slug': 'upto-500w-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': 'Up to 500W Power Supplies at HiFi Computer',
+        'description': 'Find up to 500W power supplies at HiFi Computer, ideal for basic PC builds and everyday computing needs. These power supplies offer reliable performance and efficient power delivery for systems with lower power requirements. Shop now to find the best prices and enjoy convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': '501W to 800W',
+        'slug': '501w-to-800w-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': '501W to 800W Power Supplies at HiFi Computer',
+        'description': 'Explore 501W to 800W power supplies at HiFi Computer, perfect for mid-range gaming and professional PC builds. These power supplies provide stable power delivery and efficiency for systems with moderate power needs. Shop now to find competitive prices and enjoy convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': '801W to 1000W',
+        'slug': '801w-to-1000w-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': '801W to 1000W Power Supplies at HiFi Computer',
+        'description': 'Discover 801W to 1000W power supplies at HiFi Computer, ideal for high-performance gaming rigs and professional workstations. These power supplies ensure stable and efficient power delivery for systems with higher power demands. Shop now to find the best prices and enjoy convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': 'Higher than 1000W',
+        'slug': 'higher-than-1000w-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': 'Higher than 1000W Power Supplies at HiFi Computer',
+        'description': 'Check out higher than 1000W power supplies at HiFi Computer, designed for extreme performance PC builds and demanding applications. These power supplies offer exceptional stability and efficiency for systems with the highest power requirements. Shop now to find competitive prices and enjoy convenient doorstep delivery across Bangladesh.'
+    }
+]
+
+power_supply_efficiency = [
+    {
+        'title': '80+',
+        'slug': '80plus-efficiency-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': '80+ Efficiency Power Supplies at HiFi Computer',
+        'description': 'Explore 80+ efficiency power supplies at HiFi Computer, perfect for enhancing your PC\'s energy efficiency and performance. These power supplies meet the standard 80+ certification, ensuring reliable power delivery and reduced energy consumption. Shop now to find competitive prices and enjoy convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': '80+ Titanium',
+        'slug': '80plus-titanium-efficiency-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': '80+ Titanium Efficiency Power Supplies at HiFi Computer',
+        'description': 'Discover 80+ Titanium efficiency power supplies at HiFi Computer, offering top-tier energy efficiency and performance. These power supplies meet the highest 80+ Titanium certification standards, ensuring exceptional power delivery and minimal energy waste. Shop now to find competitive prices and enjoy convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': '80+ Platinum',
+        'slug': '80plus-platinum-efficiency-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': '80+ Platinum Efficiency Power Supplies at HiFi Computer',
+        'description': 'Find 80+ Platinum efficiency power supplies at HiFi Computer, designed for high-performance and energy-efficient PC builds. These power supplies meet the stringent 80+ Platinum certification standards, ensuring superior power delivery and efficiency. Shop now to find competitive prices and enjoy convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': '80+ Gold',
+        'slug': '80plus-gold-efficiency-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': '80+ Gold Efficiency Power Supplies at HiFi Computer',
+        'description': 'Check out 80+ Gold efficiency power supplies at HiFi Computer, perfect for gamers and professionals seeking reliable and efficient power solutions. These power supplies meet the 80+ Gold certification standards, ensuring excellent power delivery and reduced energy consumption. Shop now to find competitive prices and enjoy convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': '80+ Bronze',
+        'slug': '80plus-bronze-efficiency-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': '80+ Bronze Efficiency Power Supplies at HiFi Computer',
+        'description': 'Explore 80+ Bronze efficiency power supplies at HiFi Computer, ideal for cost-effective and efficient PC builds. These power supplies meet the 80+ Bronze certification standards, providing reliable power delivery and improved energy efficiency. Shop now to find competitive prices and enjoy convenient doorstep delivery across Bangladesh.'
+    },
+    {
+        'title': '80+ White',
+        'slug': '80plus-white-efficiency-powersupply',
+        'cat_type': 'tag',
+        'priority': 4,
+        'seo_title': '80+ White Efficiency Power Supplies at HiFi Computer',
+        'description': 'Discover 80+ White efficiency power supplies at HiFi Computer, suitable for entry-level and budget-friendly PC builds. These power supplies meet the 80+ White certification standards, ensuring reliable power delivery and basic energy efficiency. Shop now to find competitive prices and enjoy convenient doorstep delivery across Bangladesh.'
+    }
+]
+
+
+
+
+POWER_SUPPLY_CATEGORIES = [
+    *power_supply_brands,
+    *power_supply_wattage,
+    *power_supply_efficiency
+]
+
+POWER_SUPPLY_CATEGORY_GROUPS = [
+    {
+        'title': 'Wattage',
+        'slug': 'ps-wattage',
+        'priority': 4
+    },
+    {
+        'title': 'Efficiency',
+        'slug': 'ps-efficiency',
+        'priority': 4
+    },
+]
