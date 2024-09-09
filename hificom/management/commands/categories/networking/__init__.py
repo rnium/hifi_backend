ffd4961d1afdaad16d58ccf856856f67a511a7a4
rony <rniumo@gmail.com>
2024-09-09 22:42:30 +0600

    router

--- hificom/management/commands/categories/networking/__init__.py
+++ hificom/management/commands/categories/networking/__init__.py
@@ -1,4 +1,6 @@
-NETWORKING_CATEGORIES = [
+from .router import ROUTER_CATEGORIES, ROUTER_CATEGORY_GROUPS
+
+main_categories = [
     {
         'title': 'Router',
         'slug': 'router',
@@ -7,17 +9,19 @@ NETWORKING_CATEGORIES = [
         'description': 'Enhance your network connectivity with advanced routers from HiFi Computer. Explore a range of models suitable for home and office use, offering high-speed internet access, seamless connectivity, and robust security features, available at competitive prices in Bangladesh. Choose from top brands and enjoy reliable performance for streaming, gaming, and productivity.',
     },
     {
-        'title': 'Pocket Router',
-        'slug': 'pocket-router',
-        'priority': 3,
-        'seo_title': 'Pocket Routers at HiFi Computer',
-        'description': 'Stay connected on the go with compact and portable pocket routers from HiFi Computer. Discover lightweight and versatile options for travel, remote work, and mobile connectivity, available at competitive prices in Bangladesh. Choose pocket routers with fast wireless speeds, long-lasting battery life, and easy setup for convenient internet access anywhere.',
-    },
-    {
         'title': 'WiFi Adapter',
         'slug': 'wifi-adapter',
         'priority': 3,
         'seo_title': 'WiFi Adapters at HiFi Computer',
         'description': 'Extend your wireless range and improve network performance with WiFi adapters from HiFi Computer. Explore a variety of USB and PCIe adapters compatible with desktops and laptops, offering fast and stable connections, available at competitive prices in Bangladesh. Choose from dual-band options, MU-MIMO technology, and easy installation for enhanced WiFi coverage and speed.',
     },
+]
+ 
+NETWORKING_CATEGORIES = [
+     *main_categories,
+     *ROUTER_CATEGORIES,
+]
+
+NETWORKING_CATEGORY_GROUPS = [
+    *ROUTER_CATEGORY_GROUPS,
 ]
---
+++ hificom/management/commands/categories/networking/router.py
@@ -0,0 +1,153 @@
+router_brands = [
+    {
+        'title': 'TP-Link',
+        'slug': 'tp-link-router',
+        'priority': 5,
+        'cat_type': 'brand',
+        'seo_title': 'TP-Link Routers at HiFi Computer',
+        'description': 'Explore reliable and high-performance TP-Link routers at HiFi Computer. Perfect for home and office use, TP-Link routers offer excellent connectivity and speed. Get the best deals on TP-Link routers in Bangladesh.'
+    },
+    {
+        'title': 'Tenda',
+        'slug': 'tenda-router',
+        'priority': 5,
+        'cat_type': 'brand',
+        'seo_title': 'Tenda Routers at HiFi Computer',
+        'description': 'Upgrade your network with Tenda routers from HiFi Computer. Known for their ease of use and robust performance, Tenda routers provide seamless internet connectivity. Shop now for the best prices in Bangladesh.'
+    },
+    {
+        'title': 'Cudy',
+        'slug': 'cudy-router',
+        'priority': 5,
+        'cat_type': 'brand',
+        'seo_title': 'Cudy Routers at HiFi Computer',
+        'description': 'Discover advanced networking with Cudy routers available at HiFi Computer. Offering reliable performance and innovative features, Cudy routers are perfect for home and business use. Find the best offers in Bangladesh.'
+    },
+    {
+        'title': 'Netis',
+        'slug': 'netis-router',
+        'priority': 5,
+        'cat_type': 'brand',
+        'seo_title': 'Netis Routers at HiFi Computer',
+        'description': 'Get connected with Netis routers from HiFi Computer. Designed for high-speed internet and stable connections, Netis routers are ideal for any networking needs. Check out our competitive prices in Bangladesh.'
+    },
+    {
+        'title': 'Ruijie',
+        'slug': 'ruijie-router',
+        'priority': 5,
+        'cat_type': 'brand',
+        'seo_title': 'Ruijie Routers at HiFi Computer',
+        'description': 'Enhance your network with Ruijie routers from HiFi Computer. Known for their advanced technology and superior performance, Ruijie routers are perfect for demanding networking environments. Shop for the best prices in Bangladesh.'
+    },
+    {
+        'title': 'D-Link',
+        'slug': 'd-link-router',
+        'priority': 5,
+        'cat_type': 'brand',
+        'seo_title': 'D-Link Routers at HiFi Computer',
+        'description': 'Explore a wide range of D-Link routers at HiFi Computer. With a reputation for quality and performance, D-Link routers provide reliable connectivity solutions for home and business. Find great deals in Bangladesh.'
+    }
+]
+
+router_types = [
+    {
+        'title': 'Wired Router',
+        'slug': 'wired-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': 'Wired Routers at HiFi Computer',
+    },
+    {
+        'title': 'Wireless N Router',
+        'slug': 'wireless-n-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': 'Wireless N Routers at HiFi Computer',
+    },
+    {
+        'title': 'Dual-Band Router',
+        'slug': 'dual-band-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': 'Dual-Band Routers at HiFi Computer',
+    },
+    {
+        'title': 'Tri-Band Router',
+        'slug': 'tri-band-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': 'Tri-Band Routers at HiFi Computer',
+    },
+    {
+        'title': 'Pocket Router',
+        'slug': 'pocket-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': 'Pocket Routers at HiFi Computer',
+    },
+    {
+        'title': 'SIM Router',
+        'slug': 'sim-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': 'SIM Routers at HiFi Computer',
+    }
+]
+
+router_speeds = [
+    {
+        'title': 'Up to 150 Mbps Router',
+        'slug': 'upto-150-mbps-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': 'Up to 150 Mbps Routers at HiFi Computer',
+    },
+    {
+        'title': '151-300 Mbps Router',
+        'slug': '151-300-mbps-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': '151-300 Mbps Routers at HiFi Computer',
+    },
+    {
+        'title': '301-600 Mbps Router',
+        'slug': '301-600-mbps-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': '301-600 Mbps Routers at HiFi Computer',
+    },
+    {
+        'title': '601-1000 Mbps Router',
+        'slug': '601-1000-mbps-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': '601-1000 Mbps Routers at HiFi Computer',
+    },
+    {
+        'title': 'Higher than 1000 Mbps Router',
+        'slug': 'higher-than-1000-mbps-router',
+        'priority': 5,
+        'cat_type': 'tag',
+        'seo_title': 'Higher than 1000 Mbps Routers at HiFi Computer',
+    }
+]
+
+
+ROUTER_CATEGORIES = [
+    *router_brands,
+    *router_types,
+    *router_speeds
+]
+
+ROUTER_CATEGORY_GROUPS = [
+    {
+        'title': 'Router Type',
+        'slug': 'router-type',
+        'priority': 4
+    },
+    {
+        'title': 'Speed',
+        'slug': 'router-speed',
+        'priority': 4
+    },
+]
