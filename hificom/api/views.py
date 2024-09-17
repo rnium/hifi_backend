53e6c382a889f54e2ccd6c60e91907bdd3efc4cf
rony <rniumo@gmail.com>
2024-09-17 15:41:34 +0600

    fix

--- hificom/api/views.py
+++ hificom/api/views.py
@@ -314,7 +314,7 @@ def dashboard_stats(request):
     data = {
         'num_products': Product.objects.count(),
         'num_users': User.objects.count(),
-        'num_orders': Order.objects.filter(status__in=['pending', 'processing']),
+        'num_orders': Order.objects.filter(status__in=['pending', 'processing']).count(),
         'num_messages': 0,
     }
     return Response(data)
